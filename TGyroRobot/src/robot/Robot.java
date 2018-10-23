3d60cff75fa1e1c7b80411ebbf721a497bcba94e
Richard McMullin <richard.e.mcmullin@gmail.com>
2018-10-23 13:28:52 -0400

    Code cleanup and formatter

--- TGyroRobot/src/robot/Robot.java
+++ TGyroRobot/src/robot/Robot.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import robot.commands.AutonomousCommand;
-import robot.oi.AutoSelector;
 import robot.oi.OI;
 import robot.subsystems.CameraSubsystem;
 import robot.subsystems.DriveSubsystem;
@@ -26,146 +25,147 @@ import robot.subsystems.PowerSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final List<TSubsystem> subsystemLs = new ArrayList<TSubsystem>();
-	
-	public static final DriveSubsystem driveSubsystem = new DriveSubsystem();
-	public static final PneumaticsSubsystem pneumaticsSubsystem = new PneumaticsSubsystem();
-	public static final PowerSubsystem powerSubsystem = new PowerSubsystem();
-	public static final CameraSubsystem cameraSubsystem = new CameraSubsystem();
+    public static final List<TSubsystem>    subsystemLs         = new ArrayList<TSubsystem>();
 
-	public static OI oi;
-	
-	private Command autoCommand;
+    public static final DriveSubsystem      driveSubsystem      = new DriveSubsystem();
+    public static final PneumaticsSubsystem pneumaticsSubsystem = new PneumaticsSubsystem();
+    public static final PowerSubsystem      powerSubsystem      = new PowerSubsystem();
+    public static final CameraSubsystem     cameraSubsystem     = new CameraSubsystem();
 
-	// Add all of the subsystems to the subsystem list
-	static {
-		subsystemLs.add(driveSubsystem);
-		subsystemLs.add(pneumaticsSubsystem);
-		subsystemLs.add(powerSubsystem);
-		subsystemLs.add(cameraSubsystem);
-	}
+    public static OI                        oi;
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		
-		oi = new OI();
-		oi.init();
+    private Command                         autoCommand;
 
-		for (TSubsystem subsystem: subsystemLs) {
-			subsystem.init();
-		}
-	}
+    // Add all of the subsystems to the subsystem list
+    static {
+        subsystemLs.add(driveSubsystem);
+        subsystemLs.add(pneumaticsSubsystem);
+        subsystemLs.add(powerSubsystem);
+        subsystemLs.add(cameraSubsystem);
+    }
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
+     */
+    @Override
+    public void robotInit() {
 
-	}
+        oi = new OI();
+        oi.init();
 
-	@Override
-	public void disabledPeriodic() {
-		oi.updatePeriodic();
-		
-		Scheduler.getInstance().run();
-		updatePeriodic();
-	}	
+        for (TSubsystem subsystem : subsystemLs) {
+            subsystem.init();
+        }
+    }
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
-	@Override
-	public void autonomousInit() {
-		
-		// Turn on the drive pids for auto
-		Robot.oi.setSpeedPidEnabled(true);
-		driveSubsystem.enableSpeedPids();
-		
-		// Reset the gyro and the encoders
-		Robot.driveSubsystem.setGyroAngle(0);
-		Robot.driveSubsystem.resetEncoders();
-				
-		// Initialize the robot command after initializing the game data
-		// because the game data will be used in the auto command.
-		autoCommand = new AutonomousCommand();
-		autoCommand.start();
-	}
+    /**
+     * This function is called once each time the robot enters Disabled mode. You
+     * can use it to reset any subsystem information you want to clear when the
+     * robot is disabled.
+     */
+    @Override
+    public void disabledInit() {
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		
-		// Update the OI before running the commands
-		oi.updatePeriodic();
-		
-		Scheduler.getInstance().run();
+    }
 
-		// Update all subsystems after running commands
-		updatePeriodic();
-	}
+    @Override
+    public void disabledPeriodic() {
 
-	@Override
-	public void teleopInit() {
-		
-		if (autoCommand != null) {
-			autoCommand.cancel();
-		}
+        oi.updatePeriodic();
 
-		// Turn off the drive PIDs
-		// Save the battery in teleop by using the 
-		// SpeedController built in braking.
-		Robot.oi.setSpeedPidEnabled(false);
-		driveSubsystem.disableSpeedPids();
+        Scheduler.getInstance().run();
+        updatePeriodic();
+    }
 
-	}
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable chooser
+     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+     * remove all of the chooser code and uncomment the getString code to get the
+     * auto name from the text box below the Gyro
+     *
+     * You can add additional auto modes by adding additional commands to the
+     * chooser code above (like the commented example) or additional comparisons to
+     * the switch structure below with additional strings & commands.
+     */
+    @Override
+    public void autonomousInit() {
 
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
-	public void teleopPeriodic() {
-		
-		// Update the OI before running the commands
-		oi.updatePeriodic();
-		
-		Scheduler.getInstance().run();
-		
-		// Update all subsystems after running commands
-		updatePeriodic();
-	}
+        // Turn on the drive pids for auto
+        Robot.oi.setSpeedPidEnabled(true);
+        driveSubsystem.enableSpeedPids();
 
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
-	}
-	
-	/**
-	 * Update periodic
-	 */
-	private void updatePeriodic() {
-		
-		// Update all subsystems
-		for (TSubsystem subsystem: subsystemLs) {
-			subsystem.updatePeriodic();
-		}
-	}
+        // Reset the gyro and the encoders
+        Robot.driveSubsystem.setGyroAngle(0);
+        Robot.driveSubsystem.resetEncoders();
+
+        // Initialize the robot command after initializing the game data
+        // because the game data will be used in the auto command.
+        autoCommand = new AutonomousCommand();
+        autoCommand.start();
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    @Override
+    public void autonomousPeriodic() {
+
+        // Update the OI before running the commands
+        oi.updatePeriodic();
+
+        Scheduler.getInstance().run();
+
+        // Update all subsystems after running commands
+        updatePeriodic();
+    }
+
+    @Override
+    public void teleopInit() {
+
+        if (autoCommand != null) {
+            autoCommand.cancel();
+        }
+
+        // Turn off the drive PIDs
+        // Save the battery in teleop by using the
+        // SpeedController built in braking.
+        Robot.oi.setSpeedPidEnabled(false);
+        driveSubsystem.disableSpeedPids();
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    @Override
+    public void teleopPeriodic() {
+
+        // Update the OI before running the commands
+        oi.updatePeriodic();
+
+        Scheduler.getInstance().run();
+
+        // Update all subsystems after running commands
+        updatePeriodic();
+    }
+
+    /**
+     * This function is called periodically during test mode
+     */
+    @Override
+    public void testPeriodic() {
+    }
+
+    /**
+     * Update periodic
+     */
+    private void updatePeriodic() {
+
+        // Update all subsystems
+        for (TSubsystem subsystem : subsystemLs) {
+            subsystem.updatePeriodic();
+        }
+    }
 }
--- TGyroRobot/src/robot/RobotMap.java
+++ TGyroRobot/src/robot/RobotMap.java
@@ -9,64 +9,66 @@ import com.torontocodingcollective.speedcontroller.TCanSpeedController.TCanSpeed
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  * <p>
- * This map is intended to define the wiring only.  Robot constants should
- * be put in {@link RobotConst}
+ * This map is intended to define the wiring only. Robot constants should be put
+ * in {@link RobotConst}
  */
 public class RobotMap {
 
+    // ******************************************
+    // Speed Controllers
+    // PWM and CAN addresses
+    // ******************************************
+    public static final int                     LEFT_DRIVE_SPEED_CONTROLLER_ADDRESS;
+    public static final TCanSpeedControllerType LEFT_DRIVE_SPEED_CONTROLLER_TYPE;
+    public static final int                     LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_ADDRESS;
+    public static final TCanSpeedControllerType LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE;
+    public static final boolean                 LEFT_DRIVE_MOTOR_ISINVERTED;
 
-	//******************************************
-	// Speed Controllers 
-	// PWM and CAN addresses and Power Ports (PDP)
-	//******************************************
-	public static final int 	     LEFT_DRIVE_SPEED_CONTROLLER_CAN_ADDRESS;          
-	public static final TCanSpeedControllerType
-	                        	     LEFT_DRIVE_SPEED_CONTROLLER_TYPE;
-	public static final int 	     LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_CAN_ADDRESS; 
-	public static final TCanSpeedControllerType
-                                     LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE;
-	public static final boolean      LEFT_DRIVE_MOTOR_ISINVERTED;
-	public static final int          LEFT_DRIVE_MOTOR_POWER_PORT;      
+    public static final int                     RIGHT_DRIVE_SPEED_CONTROLLER_ADDRESS;
+    public static final TCanSpeedControllerType RIGHT_DRIVE_SPEED_CONTROLLER_TYPE;
+    public static final int                     RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_ADDRESS;
+    public static final TCanSpeedControllerType RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE;
+    public static final boolean                 RIGHT_DRIVE_MOTOR_ISINVERTED;
 
-	public static final int 	     RIGHT_DRIVE_SPEED_CONTROLLER_CAN_ADDRESS;          
-	public static final TCanSpeedControllerType
-	                        	     RIGHT_DRIVE_SPEED_CONTROLLER_TYPE;
-	public static final int 	     RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_CAN_ADDRESS; 
-	public static final TCanSpeedControllerType
-                            	     RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE;
-	public static final boolean      RIGHT_DRIVE_MOTOR_ISINVERTED;
-	public static final int 	     RIGHT_DRIVE_MOTOR_POWER_PORT;      
-	
-	//******************************************
-	// DIO Ports
-	//******************************************
-	
-	//******************************************
-	// Pneumatics Ports
-	//******************************************
-	public static final int          SHIFTER_PNEUMATIC_PORT = 0;
+    // ******************************************
+    // Encoders
+    // ******************************************
+    public static final boolean                 LEFT_DRIVE_ENCODER_ISINVERTED;
+    public static final boolean                 RIGHT_DRIVE_ENCODER_ISINVERTED;
 
-	// Initializers if this code will be deployed to more than one
-	// robot with different mappings
-	static {
-		
-		switch (RobotConst.robot) {
+    // ******************************************
+    // Gyro Ports
+    // ******************************************
+    public static final int                     GYRO_PORT;
 
-		case 1310:
-		default:
-			LEFT_DRIVE_SPEED_CONTROLLER_CAN_ADDRESS           = 0;
-			LEFT_DRIVE_SPEED_CONTROLLER_TYPE                  = TCanSpeedControllerType.TALON_SRX;
-			LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_CAN_ADDRESS  = 2;
-			LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE         = TCanSpeedControllerType.VICTOR_SPX;
-			LEFT_DRIVE_MOTOR_ISINVERTED                       = TConst.NOT_INVERTED;
-			LEFT_DRIVE_MOTOR_POWER_PORT                       = 1;
+    // ******************************************
+    // Pneumatics Ports
+    // ******************************************
+    public static final int                     SHIFTER_PNEUMATIC_PORT = 0;
 
-			RIGHT_DRIVE_SPEED_CONTROLLER_CAN_ADDRESS          = 1;
-			RIGHT_DRIVE_SPEED_CONTROLLER_TYPE                 = TCanSpeedControllerType.TALON_SRX;
-			RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_CAN_ADDRESS = 3;
-			RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE        = TCanSpeedControllerType.VICTOR_SPX;
-			RIGHT_DRIVE_MOTOR_ISINVERTED                      = TConst.INVERTED;
-			RIGHT_DRIVE_MOTOR_POWER_PORT                      = 2;
-		}
-	}
+    // Initializers if this code will be deployed to more than one
+    // robot with different mappings
+    static {
+
+        switch (RobotConst.robot) {
+
+        case RobotConst.TEST_ROBOT:
+        default:
+            LEFT_DRIVE_SPEED_CONTROLLER_ADDRESS = 0;
+            LEFT_DRIVE_SPEED_CONTROLLER_TYPE = TCanSpeedControllerType.TALON_SRX;
+            LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_ADDRESS = 2;
+            LEFT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE = TCanSpeedControllerType.TALON_SRX;
+            LEFT_DRIVE_MOTOR_ISINVERTED = TConst.INVERTED;
+            LEFT_DRIVE_ENCODER_ISINVERTED = TConst.INVERTED;
+
+            RIGHT_DRIVE_SPEED_CONTROLLER_ADDRESS = 1;
+            RIGHT_DRIVE_SPEED_CONTROLLER_TYPE = TCanSpeedControllerType.TALON_SRX;
+            RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_ADDRESS = 7;
+            RIGHT_DRIVE_FOLLOWER_SPEED_CONTROLLER_TYPE = TCanSpeedControllerType.VICTOR_SPX;
+            RIGHT_DRIVE_MOTOR_ISINVERTED = TConst.NOT_INVERTED;
+            RIGHT_DRIVE_ENCODER_ISINVERTED = TConst.NOT_INVERTED;
+
+            GYRO_PORT = 0;
+        }
+    }
 }
